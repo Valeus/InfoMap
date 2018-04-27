04212311002306c5396e18b6b8f26ab278555f84
valeus <edwardmig42@gmail.com>
2018-04-27 11:51:48 -0400

    4-27-18

--- app/src/main/java/com/example/a10019356/infomap/MainActivity.java
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.a10019356.infomap;
 
-import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
 
@@ -19,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_main);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         mapFragment.getMapAsync(this);
-//iutfvhlgful
+
     }
 
     @Override
@@ -27,8 +26,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    public interface TileProvider{
-
-    }
 
 }
