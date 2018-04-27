637465c5b90e1dc3fa53f2ebe01f5cabdcb0f866
Edward Migliore <10019356@sbschools.org>
2018-04-27 11:17:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.a10019356.infomap;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    public interface TileProvider{
+
+    }
+
+}
