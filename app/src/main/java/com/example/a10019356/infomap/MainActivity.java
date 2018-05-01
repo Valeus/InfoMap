91aa48c4ecdb0cbdf0622e621f35862001f85bc8
valeus <edwardmig42@gmail.com>
2018-05-01 11:52:59 -0400

    5-1-18

--- app/src/main/java/com/example/a10019356/infomap/MainActivity.java
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a10019356.infomap;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -10,10 +11,11 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
 
     SupportMapFragment mapFragment;
-    int lat,lng;
+    GoogleMap googleMap;
+    double lat, lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +24,22 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         mapFragment.getMapAsync(this);
 
-
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-        Marker melbourne = googleMap.addMarker(new MarkerOptions().position(MELBOURNE).title("Melbourne").snippet("Population: 4,137,400"));
-        melbourne.showInfoWindow();
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                lat = latLng.latitude;
+                lng = latLng.longitude;
+            }
+        });
+
+        final LatLng touch = new LatLng(lat, lng);
+        Marker test = googleMap.addMarker(new MarkerOptions().position(touch).title("TEST").snippet("Population: 1"));
+        test.showInfoWindow();
     }
 
-
 }
+
