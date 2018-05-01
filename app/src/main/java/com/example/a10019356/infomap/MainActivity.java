113cb88d6853592809717f53efd8fc4c15baa2ad
valeus <edwardmig42@gmail.com>
2018-05-01 08:33:05 -0400

    5-1-18

--- app/src/main/java/com/example/a10019356/infomap/MainActivity.java
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.a10019356.infomap;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     SupportMapFragment mapFragment;
+    int lat,lng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+        Marker melbourne = googleMap.addMarker(new MarkerOptions().position(MELBOURNE).title("Melbourne").snippet("Population: 4,137,400"));
+        melbourne.showInfoWindow();
     }
 
 
