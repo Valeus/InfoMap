e3c4e39a310893ca94da9f79724d87145c51cea3
valeus <edwardmig42@gmail.com>
2018-05-03 08:40:08 -0400

    5-3-18

--- app/src/main/java/com/example/a10019356/infomap/MainActivity.java
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.a10019356.infomap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,7 +19,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
 
     SupportMapFragment mapFragment;
-    GoogleMap googleMap;
+    GoogleMap map;
     double lat, lng;
 
     @Override
@@ -23,18 +28,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
         mapFragment.getMapAsync(this);
+        mapFragment.getView().setClickable(true);
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                lat = latLng.latitude;
-                lng = latLng.longitude;
-            }
-        });
+        map = googleMap;
 
         final LatLng touch = new LatLng(lat, lng);
         Marker test = googleMap.addMarker(new MarkerOptions().position(touch).title("TEST").snippet("Population: 1"));
