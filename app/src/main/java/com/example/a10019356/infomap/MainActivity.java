1807b84ce5bfd3973e964f6aa86eb3dca32be1b7
valeus <edwardmig42@gmail.com>
2018-05-03 11:51:39 -0400

    5-3-18

--- app/src/main/java/com/example/a10019356/infomap/MainActivity.java
+++ app/src/main/java/com/example/a10019356/infomap/MainActivity.java
@@ -1,45 +1,115 @@
 package com.example.a10019356.infomap;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
-    SupportMapFragment mapFragment;
-    GoogleMap map;
-    double lat, lng;
+    MapView mapView;
+    static final String MAPVIEW_KEY = "Kiddo";
+    GoogleMap googleMap;
+    int markerCount= 0;
+    Marker test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
-        mapFragment.getMapAsync(this);
-        mapFragment.getView().setClickable(true);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_KEY);
+        }
+        mapView = findViewById(R.id.id_map);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                if(markerCount==0) {
+                    test = googleMap.addMarker(new MarkerOptions().position(latLng).title("TEST").snippet("Population: 1"));
+                    test.showInfoWindow();
+                    markerCount++;
+                }
+                if(markerCount==1){
+                    test.remove();
+                    test = googleMap.addMarker(new MarkerOptions().position(latLng).title("TEST").snippet("Population: 1"));
+                    test.showInfoWindow();
+                }
+
+
+
+            }
+        });
 
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-
-        final LatLng touch = new LatLng(lat, lng);
-        Marker test = googleMap.addMarker(new MarkerOptions().position(touch).title("TEST").snippet("Population: 1"));
-        test.showInfoWindow();
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
 
 }
 
